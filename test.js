3d9a3a3ca8d6b4ca817d672a0531a54a81b50f58
zia0001 <ziawc0001@gmail.com>
2024-11-10 00:39:04 +0500

    variables in js

---
+++ test.js
@@ -0,0 +1,16 @@
+
+
+const Id = 2333
+let Name = "zia"
+var company = "company name"
+city = "xyz"
+let postOffice;
+
+// Id = 345      // cannot reassign a const value
+Name = "ahmad"
+company = "softedge"
+city = "wah cantt"
+
+console.table([Name, company, city, postOffice]);
+
+// variables
